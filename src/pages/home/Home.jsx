ba3c3954d5529915622790bce1c6e8b74b6b6b36
amitk0091 <akn778811@gmail.com>
2024-04-25 19:34:00 +0530

    working email link verification

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -5,13 +5,32 @@ import { auth } from '../../config/config_firebase';
 const Home = () => {
     const location = useLocation();
     console.log(location);
-    useEffect (()=>{
-        console.log('here');
-        const sendVerifyReq = async() =>{
-            const resp = await fetch(`https://fir-authentication-e63f1.firebaseapp.com/__/auth/action${location.search}`)
+    useEffect(() => {
+        const urlParams = new URLSearchParams(window.location.search);
+
+        // Get the values of the 'oobCode' and 'apiKey' parameters
+        const oobCodeval = urlParams.get('oobCode');
+        const apiKey = urlParams.get('apiKey');
+
+        const url = `https://identitytoolkit.googleapis.com/v1/accounts:update?key=${apiKey}`;
+        // console.log(oobCodeval,apiKey);
+        const requestOptions = {
+            method: 'POST', // or 'GET', 'PUT', 'DELETE', etc.
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                oobCode: oobCodeval
+            })
+        };
+
+        // console.log('here');
+        const sendVerifyReq = async () => {
+            const resp = await fetch(url, requestOptions);
+            console.log(resp);
         }
         sendVerifyReq();
-    },[])
+    }, [])
 
 
     return (
